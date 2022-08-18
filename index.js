28e7c2b82da1b7e7d61a31a61bf4b07482846795
mwendamwenda <benmwenda83@gmail.com>
2022-08-18 15:51:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+let  companyName = "Scuber";
+function companyName(){
+    const companyName = "Scuber";
+ }
+
+ let mostProfitableNeighborhood = "Chelsea";
+ function mostProfitableNeighborhood(){
+    let mostProfitableNeighborhood = "Chelsea";
+
+ }
+ let companyCeo = "Susan Smith"
+ function companyCeo (){
+    let companyCeo = "Susan Smith"
+ }
